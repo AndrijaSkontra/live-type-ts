5b27501e5d6946b98f2b11d7f674d921a557abb8
AndrijaSkontra <andrija.skontra@kodelab.hr>
2025-01-06 22:22:50 +0100

    add: header

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "leaderboard/index.html"),
+        leaderboard: resolve(__dirname, "leaderboard/index.html"),
+        practice: resolve(__dirname, "practice/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+        play: resolve(__dirname, "play/index.html"),
       },
     },
   },
